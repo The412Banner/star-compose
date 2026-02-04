131f1c7f6adb30f20a3a56b0010847723dd8f830
kalturu321-svg <kalturu321@gmail.com>
2026-02-04 09:41:55 +0800

    Update DefaultVersion.java

--- app/src/main/java/com/winlator/cmod/core/DefaultVersion.java
+++ app/src/main/java/com/winlator/cmod/core/DefaultVersion.java
@@ -6,7 +6,10 @@ public abstract class DefaultVersion {
     public static final String FEXCORE = "2508";
     public static final String WRAPPER = "System";
     public static final String WRAPPER_ADRENO = "turnip25.1.0";
+    public static final String VIRGL = "23.1.9";
+    public static final String LLVMPIPE = "25.0.0";
     public static final String DXVK = GPUInformation.getRenderer(null, null).contains("Mali") ? "1.10.3" : "2.3.1";
     public static final String D8VK = "1.0";
     public static final String VKD3D = "None";
+
 }
