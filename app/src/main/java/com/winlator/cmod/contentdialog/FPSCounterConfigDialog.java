e2f8ba84f050b80457c60f5cba4b3ffd14c7cd93
kalturu321-svg <kalturu321@gmail.com>
2026-02-09 08:14:56 +0800

    Update FPSCounterConfigDialog.java

--- app/src/main/java/com/winlator/cmod/contentdialog/FPSCounterConfigDialog.java
+++ app/src/main/java/com/winlator/cmod/contentdialog/FPSCounterConfigDialog.java
@@ -1,10 +1,8 @@
 package com.winlator.cmod.contentdialog;
 
 import android.content.Context;
-import android.view.View;
 import android.widget.CheckBox;
 import com.winlator.cmod.R;
-import com.winlator.cmod.container.Container;
 import com.winlator.cmod.core.KeyValueSet;
 
 public class FPSCounterConfigDialog extends ContentDialog {
@@ -13,9 +11,12 @@ public class FPSCounterConfigDialog extends ContentDialog {
     private final CheckBox cbShowGPULoad;
     private final CheckBox cbShowRAM;
     private final CheckBox cbShowRenderer;
+    private String configString;
 
-    public FPSCounterConfigDialog(Context context, Container container) {
+    // We now pass the String config instead of the Container object
+    public FPSCounterConfigDialog(Context context, String initialConfig) {
         super(context, R.layout.fps_counter_config_dialog);
+        this.configString = initialConfig;
         setTitle("FPS Counter Settings");
 
         cbShowFPS = findViewById(R.id.CBShowFPS);
@@ -24,7 +25,8 @@ public class FPSCounterConfigDialog extends ContentDialog {
         cbShowRAM = findViewById(R.id.CBShowRAM);
         cbShowRenderer = findViewById(R.id.CBShowRenderer);
 
-        KeyValueSet config = new KeyValueSet(container.getFPSCounterConfig());
+        // Load values from the provided string
+        KeyValueSet config = new KeyValueSet(initialConfig);
         cbShowFPS.setChecked(config.get("showFPS").equals("1"));
         cbShowCPULoad.setChecked(config.get("showCPULoad").equals("1"));
         cbShowGPULoad.setChecked(config.get("showGPULoad").equals("1"));
@@ -32,8 +34,22 @@ public class FPSCounterConfigDialog extends ContentDialog {
         cbShowRenderer.setChecked(config.get("showRenderer").equals("1"));
 
         setOnConfirmCallback(() -> {
-            config.put("showFPS", cbShowFPS.isChecked() ? "1" : "0");
-            config.put("showCPULoad", cbShowCPULoad.isChecked() ? "1" : "0");
+            // Update the local configString when OK is pressed
+            KeyValueSet newConfig = new KeyValueSet();
+            newConfig.put("showFPS", cbShowFPS.isChecked() ? "1" : "0");
+            newConfig.put("showCPULoad", cbShowCPULoad.isChecked() ? "1" : "0");
+            newConfig.put("showGPULoad", cbShowGPULoad.isChecked() ? "1" : "0");
+            newConfig.put("showRAM", cbShowRAM.isChecked() ? "1" : "0");
+            newConfig.put("showRenderer", cbShowRenderer.isChecked() ? "1" : "0");
+            this.configString = newConfig.toString();
+        });
+    }
+
+    // Getter to retrieve the modified string back in the Fragment
+    public String getConfigString() {
+        return configString;
+    }
+}
             config.put("showGPULoad", cbShowGPULoad.isChecked() ? "1" : "0");
             config.put("showRAM", cbShowRAM.isChecked() ? "1" : "0");
             config.put("showRenderer", cbShowRenderer.isChecked() ? "1" : "0");
