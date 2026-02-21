7e3b13d0126db7e9c7a3f5b032718e6793f81367
kalturu321-svg <kalturu321@gmail.com>
2026-02-22 07:50:06 +0800

    Update ShortcutSettingsDialog.java

--- app/src/main/java/com/winlator/cmod/contentdialog/ShortcutSettingsDialog.java
+++ app/src/main/java/com/winlator/cmod/contentdialog/ShortcutSettingsDialog.java
@@ -16,6 +16,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.FrameLayout;
@@ -789,5 +790,6 @@ public class ShortcutSettingsDialog extends ContentDialog {
         update.run();
     }
 }
+
 
 
